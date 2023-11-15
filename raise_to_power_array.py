ea5c98a513fc1ec7224075615f6c43156e7ff996
Eugenio Piasini <eugenio.piasini@gmail.com>
2023-11-15 15:27:46 +0100

    Make sure the data array can be imported from other files

--- raise_to_power_array.py
+++ raise_to_power_array.py
@@ -1,10 +1,5 @@
 #!/usr/bin/env python
 import argparse
-import logging
-
-logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s:%(message)s')
-
-
 parser = argparse.ArgumentParser(description="Take in an integer N, defined as the i-th element of a pre-defined data array (N=data[i]) and raise it to the power a. By default, this will compute the square (a=2).")
 
 parser.add_argument('input_id', metavar='i', type=int,
@@ -13,13 +8,19 @@ parser.add_argument('input_id', metavar='i', type=int,
 parser.add_argument('--power', metavar='a', type=float, default=2,
                     help="Power to which you want to raise the integer (default: 2)")
 
+data = np.arange(-10, 11, 1, dtype=int)
+
 
 if __name__ == '__main__':
 
+    import logging
     import numpy as np
 
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s:%(message)s')
+
+
+
     # hardcoded data array
-    data = np.arange(-10, 11, 1, dtype=int)
     logging.info(f"The hardcoded data array is defined as {data}")
     
     args = parser.parse_args()
@@ -28,3 +29,5 @@ if __name__ == '__main__':
     result = np.power(args.input_id, args.power)
 
     print(f"Result: {result}")
+
+    print("This is another line of text printed to the script's output to make things a bit harder")
